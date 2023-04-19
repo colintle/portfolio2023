e76f9c1404c6f43b8fc4d9a7d3bae190b4dcea1e
Colin Le <Colin.Le@ucf.edu>
2023-04-19 10:01:19 -0400

    Add splash screen

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import React, { useState, useEffect } from "react";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <div className="app">
+      {showSplash && (
+        <div className="splash-screen">
+          <h1>Hello, World</h1>
+        </div>
+      )}
+      {!showSplash && 
+      <div className="content">
+        <Component {...pageProps} />
+      </div>}
+    </div>
+  );
 }
+
+export default MyApp;
