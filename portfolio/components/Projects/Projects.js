a4cbef4156d22bbd63439c93f2c234b34b39205c
Colin Le <colintranle123@gmail.com>
2023-05-06 20:04:49 -0400

    Add images to projects and complete description of each

--- portfolio/components/Projects/Projects.js
+++ portfolio/components/Projects/Projects.js
@@ -5,7 +5,7 @@ function Projects() {
     <div className='md:h-screen flex justify-center'>
       <div className='w-3/4 md:w-1/2'>
         <h1 className='w-fit m-auto text-2xl lg:text-3xl xl:text-4xl text-center my-5'>
-          Projects
+          Featured Projects
           <hr className='w- h-1 mx-auto my-4 bg-gradient-to-b from-sky-600 to-purple-700 border-0 rounded'></hr>
         </h1>
         <div className='text-mg lg:text-lg xl:text-xl'>
--- portfolio/data/Data.js
+++ portfolio/data/Data.js
@@ -2,35 +2,35 @@ export const PROJECTS = [{
     name: "J.P. Morgan Code for Good 2022",
     description:
       "In 24 hours, we developed a web app for Tarrant To & Through Partnership (T3) to provide the ultimate experience for the student and family to be more educated about the possibility of pursuing higher-level education. A student and family may fill out a survey to display personalized information of schools and scholarships or chat with T3acherBot, our chatting bot, to answer any questions/concerns.",
-    image: "",
+    image: "./images/cog.png",
     note: "The GitHub repo is private, so please contact me for more information about this project!",
   },
   {
     name: "SwampHacks 2023",
     description:
       "Let Adventure Roulette be your guide to the unknown! Explore new locations near you and visit them if it piques your interest! The technologies used in this project were OpenAI, ReactJS, ExpressJS, NodeJS, and Google Maps API",
-    image: "",
+    image: "./images/adventure.png",
     github: "https://github.com/colintle/swamphack-2023",
   },
   {
     name: "Spotify Playlist Generator",
     description:
       "Using the Spotify API, this app allows the user to generate playlists with a click of a button. Check it out!",
-    image: "",
+    image: "./images/spotify.png",
     github: "https://github.com/colintle/Spotify-Playlist-Generator",
   },
   {
     name: "College Event Website",
     description:
       "This full-stack project was done in COP 4710 to replicate the functionality of the University of Central Florida's Event Website.The technologies involved with this project were MySQL, ReactJS, ExpressJS, and NodeJS",
-    image: "",
+    image: "./images/collegeevent.png",
     github: "https://github.com/colintle/CollegeEventWebsite",
   },
   {
     name: "Memories Social Media",
     description:
       "Inspired by the YouTube channel JavaScript Mastery, this full-stack project allows for users to share their memories and interact with memories from others! The technologies involved with this project were MongoDB, ReactJS, ExpressJS, and NodeJS",
-    image: "",
+    image: "./images/memories.png",
     github: "https://github.com/colintle/memories",
   },
 ]
