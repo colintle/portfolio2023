31e83796d495bc7473f3a64900d490d54e77949a
Colin Le <colintranle123@gmail.com>
2023-05-29 10:56:17 -0400

    Add margin bottom to close button

--- portfolio/components/Projects/Projects.js
+++ portfolio/components/Projects/Projects.js
@@ -121,7 +121,7 @@ function Projects() {
           isOpen={showModal}
           onRequestClose={resetModal}
           className="animate__animated animate__fadeIn bg-black bg-opacity-25 backdrop-blur-sm h-screen flex flex-col justify-center items-center">
-            <div className='w-80 lg:w-96 flex flex-row-reverse'>
+            <div className='w-80 lg:w-96 flex flex-row-reverse mb-2'>
               <button onClick={resetModal} className='hover:-translate-y-1 transition-transform cursor-pointer'>
                 <span className="text-transparent bg-clip-text bg-gradient-to-b from-sky-600 to-purple-700 h-7 w-7 text-2xl flex justify-center items-center">
                   <i class="fa-solid fa-xmark"></i>
